edbd2c708165d979ff64303bddf55534ddea29a9
Steven Shin <dks@3disc.com>
2025-04-11 22:59:45 +0900

    .

--- scripts/health.sh
+++ scripts/health.sh
@@ -21,7 +21,7 @@ do
   then # $UP_COUNT >= 1 ("real" 문자열이 있는지 검증)
     echo "> Health Check 성공"
     switch_proxy
-    brek
+    break
   else
     echo "> Health Check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다"
     echo "> Health Check: ${RESPONSE}"
